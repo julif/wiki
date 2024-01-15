29db2e2412eb209013aede7404c59205dbd26829
Julif <julif-@live.com>
2024-01-15 17:55:22 -0300

    wiki2

--- personajes.js
+++ personajes.js
@@ -1,10 +1,11 @@
 var personajes = [
   { nombre : 'Oroboros', thumb:'img/thumbs/thumb_oroboros.png', raza: ['entidad cosmica'],},
   { nombre : 'Asariel',  thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
-  { nombre : 'Theia', thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
+  { nombre : 'Theia', thumb:'img/thumbs/thumb_theia.png', raza: ['entidad cosmica']},
   { nombre : 'Nariel', thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
-  { nombre : 'Kho', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']},
-  { nombre : 'Deva', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']},
-  { nombre : 'Lith', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']}
+  /* raza: ['elfo celestial','demonio'] */
+  { nombre : 'Kho', thumb:'img/thumbs/thumb_kho.png', raza: ['elfo celestial']},
+  { nombre : 'Deva', thumb:'img/thumbs/thumb_deva.png', raza: ['elfo celestial']},
+  { nombre : 'Lith', thumb:'img/thumbs/thumb_lith.png', raza: ['elfo celestial']}
 ]; 
 
