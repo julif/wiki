d7efbbe734f5399f548e1a460ffccf3a225bef8b
Julif <julif-@live.com>
2024-01-15 01:27:54 -0300

    wiki

---
+++ personajes.js
@@ -0,0 +1,10 @@
+var personajes = [
+  { nombre : 'Oroboros', thumb:'img/thumbs/thumb_oroboros.png', raza: ['entidad cosmica'],},
+  { nombre : 'Asariel',  thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
+  { nombre : 'Theia', thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
+  { nombre : 'Nariel', thumb:'img/thumbs/thumb_default.png', raza: ['entidad cosmica']},
+  { nombre : 'Kho', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']},
+  { nombre : 'Deva', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']},
+  { nombre : 'Lith', thumb:'img/thumbs/thumb_default.png', raza: ['elfo celestial','demonio']}
+]; 
+
