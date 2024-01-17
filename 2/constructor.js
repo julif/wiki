cf66f97669ca76a04460b6df7214b4a15f74782e
Julif <julif-@live.com>
2024-01-17 20:46:38 -0300

    0

---
+++ 2/constructor.js
@@ -0,0 +1,7 @@
+document.getElementById("sidebar").innerHTML = '<img src="logo_3.png">';
+document.getElementById("sidebar").innerHTML += '<sidebar_nav><a href="index.html"> Inicio </a><a href="personajes.html"> Personajes </a> <a href="eventos.html" > Eventos </a> </sidebar_nav><separador></separador>';
+
+     
+
+
+
